1abce46fdb2acaf3c44af50c53cebc4f1d530c9d
Joseph Hinson <joseph@outthink.co>
2025-02-27 20:40:01 -0500

    reorganized form fields for quick posts page

--- templates/add-quick-post.php
+++ templates/add-quick-post.php
@@ -117,7 +117,7 @@ $selected = get_option('qp_selected_template'); ?>
 	</div>
 	<div id="page-options">
 		<div class="misc-pub-section misc-pub-section-template for-page">
-			<?if ( 0 != count( get_page_templates() ) ) {
+			<?php if ( 0 != count( get_page_templates() ) ) {
 					$template = !empty($post->page_template) ? $post->page_template : false;
 					?>
 			<label for="page_template"><?php _e('Page Template') ?></label>
@@ -125,8 +125,7 @@ $selected = get_option('qp_selected_template'); ?>
 			<option value='default'><?php _e('Default Template'); ?></option>
 			<?php page_template_dropdown($template); ?>
 			</select>
-			<?php
-				} ?>
+			<?php } ?>
 		</div>
 	
 		<div class="misc-pub-section misc-pub-section-parent for-page">
@@ -170,59 +169,73 @@ $selected = get_option('qp_selected_template'); ?>
 	<table cellspacing="2" cellpadding="5" style="width: 100%;" class="form-table">
 		<tbody>
 	<?php 
-
 //		creating new option to loop through mroe easily...since i'm so dumb
 // This pulls in the "selected" template
 if (empty($options[$selected])) {
 	echo '<div class="updated below-h2"><p>It doesn\'t look like you\'ve created a post template. Jump over to <a href="admin.php?page=qp-templates">the template creator and make one now.</a></div>';
+	return;
 }
-	$newoptions[] = $options[$selected];
 
-	for($i = 0; $i < count($newoptions); ++$i) {
-		$option = $newoptions[$i];
-		if ($option['title']) { ?>
-			<tr class="form-field">
-					<th valign="top" scope="row"><label>Title</label></th>
-					<td><input type="text" style="width: 95%;" value="" size="50" class="code" name="title[]"></td>
-			</tr>				
-<?php 	}
-		if ($option['content']) { ?>
-			<tr class="form-field">
-				<th valign="top" scope="row"><label>Content</label></th>
-				<td><textarea style="width: 95%;" rows="5" cols="50" name="content[]"></textarea></td>
-			</tr>
-<?php 	}
-		if ($option['ext_url']) { ?>
-			<tr class="form-field">
-				<th valign="top" scope="row"><label>URL:</label></th>
-				<td><input type="text" style="width: 95%;" class="code" name="ext_url[]"></td>
-			</tr>
-			
-<?php	}
-		if ($option['date']) { ?>
-			<tr class="form-field">
-				<th valign="top" scope="row"><label>Date</label></th>
-				<td><input type="text" style="width: 95%;" value="" size="50" class="code" name="date[]"><br>
-					<small>(Accepts logical dates)</small></td>
-			</tr>
-	<?php	}
-			// if we have tags, spit out that form:
-			if ($option['tags']) { ?>
-				<tr class="form-field">
-					<th valign="top" scope="row"><label>Tags</label></th>
-					<td><input type="text" style="width: 95%;" value="" size="50" class="code" name="tags[]">,
-						<small>(comma separated)</small>
-					</td>
-				</tr>
-<?php 	} if ($option['cfields']) {
-	foreach ($option['cfields'] as $key => $value) { ?>
+$template = $options[$selected];
+$field_definitions = [
+	'title' => [
+		'label' => 'Title',
+		'type' => 'input',
+		'extra' => ''
+	],
+	'content' => [
+		'label' => 'Content',
+		'type' => 'textarea',
+		'extra' => ''
+	],
+	'ext_url' => [
+		'label' => 'URL',
+		'type' => 'input',
+		'extra' => ''
+	],
+	'date' => [
+		'label' => 'Date',
+		'type' => 'input',
+		'extra' => '<br><small>(Accepts logical dates)</small>'
+	],
+	'tags' => [
+		'label' => 'Tags',
+		'type' => 'input',
+		'extra' => ',<small>(comma separated)</small>'
+	]
+];
+
+foreach ($field_definitions as $field => $config) {
+	if (!empty($template[$field])) {
+		?>
 		<tr class="form-field">
-			<th valign="top" scope="row"><label><?php echo esc_html($value); ?></label></th>
-			<td><input type="text" style="width: 95%;" value="" size="50" class="code" name="<?php echo esc_attr($key).'[]'; ?>">
+			<th valign="top" scope="row"><label><?php echo $config['label']; ?></label></th>
+			<td>
+				<?php if ($config['type'] === 'textarea'): ?>
+					<textarea style="width: 95%;" rows="5" cols="50" name="<?php echo $field; ?>[]"></textarea>
+				<?php else: ?>
+					<input type="text" style="width: 95%;" value="" size="50" class="code" name="<?php echo $field; ?>[]">
+				<?php endif; ?>
+				<?php echo $config['extra']; ?>
 			</td>
 		</tr>
-<?php  }
+		<?php
+	}
 }
+
+if (!empty($template['cfields'])) {
+	foreach ($template['cfields'] as $key => $value) {
+		?>
+		<tr class="form-field">
+			<th valign="top" scope="row"><label><?php echo esc_html($value); ?></label></th>
+			<td>
+				<input type="text" style="width: 95%;" value="" size="50" class="code" name="<?php echo esc_attr($key); ?>[]">
+			</td>
+		</tr>
+		<?php
+	}
+}
+
 ?>
 	<tr>
 		<td colspan="2" align="right">
